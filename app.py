8c370c525b1446cc3c34a7fa91ab1a6f903d25b5
Phantom <srijayavel.k@gmail.com>
2026-06-04 12:18:49 +0530

    Added stock data fetching using yfinance

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import yfinance as yf
+
 st.title("Stock Insight Dashboard")
-st.write("My first stock market dashboard project")
+ticker = "RELIANCE.NS"
+stock = yf.Ticker(ticker)
+info = stock.info
+st.subheader("Company Information")
+st.write(f"Name: {info.get('longName', 'N/A')}")
+st.write(f"Current Price: ₹{info.get('currentPrice', 'N/A')}")
+tricker = st.text_input("Enter Stock Ticker", value="RELIANCE.NS")
